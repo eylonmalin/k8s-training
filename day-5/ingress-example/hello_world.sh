149c94ca975338611eb21c3ca8c330dbe4c7d4b0
Eylon Malin <6641032+eylonmalin@users.noreply.github.com>
2022-03-07 15:41:24 +0200

    add ingress examples

---
+++ day-5/ingress-example/hello_world.sh
@@ -0,0 +1,4 @@
+echo "<html><body>hello</body></html>" > /usr/share/nginx/html/hello.html
+echo "<html><body>hello index</body></html>" > /usr/share/nginx/html/index.html
+echo "<html><body>world</body></html>" > /usr/share/nginx/html/world.html
+echo "<html><body>world index</body></html>" > /usr/share/nginx/html/index.html
